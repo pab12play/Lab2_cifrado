e78eb39921ed3556e83d2372ac4d7ae39d5320d1
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-05 14:31:57 -0600

    Agregada, funcion para calcular numeros primos

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -52,6 +53,41 @@ namespace RSA
             {
                 Console.WriteLine("Por favor ingrese una opción correcta. Consulte la opción 'help' para ayuda");
             }
+            Console.ReadLine();
+        }
+
+        static int generatePrime()
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1000000, 10000000);
+            while (!isPrime(number))
+            {
+                number = number+1;
+            } 
+            return number;
+        }
+
+        static bool isPrime(int candidate)
+        {
+            if ((candidate & 1) == 0)
+            {
+                if (candidate == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
         }
     }
 }
