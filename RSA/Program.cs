d570c0577124b6c7f0212d1c1c235ba149da7ef7
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-05 10:17:54 -0600

    Agregado, menu con parametros

--- RSA/Program.cs
+++ RSA/Program.cs
@@ -10,6 +10,48 @@ namespace RSA
     {
         static void Main(string[] args)
         {
+            if (args.Length == 1 && args[0].ToLower().Equals("help"))
+            {
+                Console.WriteLine("Hola");
+                Console.WriteLine("Uso: .\\RSA.exe [-d] [-c] [-f <archivo>]");
+                Console.WriteLine("");
+                Console.WriteLine("\t-d\tDescifra un archivo");
+                Console.WriteLine("\t-c\tCifra un archivo");
+                Console.WriteLine("\t-f\tEspecifica la ruta y nombre del archivo");
+            }
+            else if (args.Length == 3)
+            {
+                switch (args[0].ToLower())
+                {
+                    case "-d":
+                        if (args[1].ToLower().Equals("-f"))
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Por favor ingrese una opción correcta. Consulte la opción 'help' para ayuda");
+                        }
+                        break;
+                    case "-c":
+                        if (args[1].ToLower().Equals("-f"))
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Por favor ingrese una opción correcta. Consulte la opción 'help' para ayuda");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Por favor ingrese una opción correcta. Consulte la opción 'help' para ayuda");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Por favor ingrese una opción correcta. Consulte la opción 'help' para ayuda");
+            }
         }
     }
 }
