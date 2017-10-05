fd474568c80bf9bca98f6e2c50a6e11e3ed724af
Pablo Pineda <pablo12.izq@gmail.com>
2017-10-05 16:23:54 -0600

    Creada clase llave privada

---
+++ RSA/PrivateKey.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class PrivateKey
+    {
+        int phi;
+        double d;
+        int k;
+        int e;
+
+        public PrivateKey(int phi, int e)
+        {
+            this.Phi = phi;
+            this.E = e;
+            k = 2;
+            D = (1 + (k * phi)) / e;
+        }
+
+        public int Phi { get => phi; set => phi = value; }
+        public int E { get => e; set => e = value; }
+        public int K { get => k; set => k = value; }
+        public double D { get => d; set => d = value; }
+    }
+}
